47b09c446097278305cb44d5abee171350cc88a0
Kevin4Calderon <91550461+Kevin4Calderon@users.noreply.github.com>
2021-11-24 19:36:59 -0600

    Update Funcion2.m

--- Funcion2.m
+++ Funcion2.m
@@ -1,8 +1,8 @@
 % Script de Octave
 % Titulo      :Funciones trascendentes
 % Author      :Kevin Yoan Calderón García
+% Description :Script para graficar una funciones trascendentes
 % Date        :20211123
 % Version     :1
 % Usage       :octave> cd /path/
 % Notes       :Requiere aplicacion octave
-
