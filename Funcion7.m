1faf438d342824f15ab5d69127ddd04ac0d3a70b
Kevin4Calderon <91550461+Kevin4Calderon@users.noreply.github.com>
2021-11-24 21:04:18 -0600

    Create Funcion7.m

---
+++ Funcion7.m
@@ -0,0 +1,18 @@
+% Script de Octave
+% Titulo      :Funcion 7
+% Author      :Kevin Yoan Calderón García
+% Description :Script para graficar una funciones trascendentes
+% Date        :20211123
+% Version     :1
+% Usage       :octave> cd /path/
+% Notes       :Requiere aplicacion octave
+
+clear
+pkg load symbolic
+v = -10:1:10;
+cv = sin(v);
+plot(v, cv);
+title(['La funcion 7 es sobreyectiva']);
+disp("Es subrayectiva ya que ningun elemento del codominio sobra respecto al dominio");
+disp("Es inyectiva ya que al trazar una rectar hacia el eje x, corta mas de un punto la recta");
+disp("Por lo tanto no puede ser biyectiva ya que no cumple con ambas clasificaciones");
