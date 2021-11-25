348374f5170d7da7e6fd804268020015f14be823
Kevin4Calderon <91550461+Kevin4Calderon@users.noreply.github.com>
2021-11-24 19:34:34 -0600

    Update Funcion1.m

--- Funcion1.m
+++ Funcion1.m
@@ -1,12 +1,15 @@
 % Script de Octave
 % Titulo      :Funciones trascendentes
 % Author      :Kevin Yoan Calderón García
+% Description :Script para graficar una funciones trascendentes
 % Date        :20211123
 % Version     :1
 % Usage       :octave> cd /path/
 % Notes       :Requiere aplicacion octave
 
 clear
+pkg load symbolic
+syms x
 x = -10:1:10;
 fx = ((3./7).^(x));
 plot(x, fx);
