e50b72fb92587bd2a3ba7f0ff0eaa6f350f9244c
Kevin4Calderon <91550461+Kevin4Calderon@users.noreply.github.com>
2021-11-24 20:32:13 -0600

    Update Funcion3.m

--- Funcion3.m
+++ Funcion3.m
@@ -13,7 +13,7 @@ syms x
 x = -10:1:10;
 fx = exp(x);
 plot(x, fx);
-title(["La funcion 3 es biyectiva']);
-disp("Es subrayectiva ya que ningun elemento del codominio sobra respecto al dominio");
+title(['La funcion 4 es biyectiva']);
+disp("Es subrayectiva por que ningun elemento del codominio sobra respecto al dominio");
 disp("Es inyectiva ya que al trazar una rectar hacia el eje x, no corta mas de un punto la recta");
-disp("Por lo tanto es biyectiva por que cumple con ambas clasificaciones");
+disp("Por lo tanto es biyectiva ya que cumple con las anteriores clasificaciones");
