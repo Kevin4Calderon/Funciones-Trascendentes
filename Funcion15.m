d41889bf7a0884e83ddab0c4a9f0718d2809cd6a
Kevin4Calderon <91550461+Kevin4Calderon@users.noreply.github.com>
2021-11-24 21:58:04 -0600

    Update Funcion15.m

--- Funcion15.m
+++ Funcion15.m
@@ -11,7 +11,7 @@ clear
 x = -2:0.1:2;
 y = exp (9*x+18);
 plot(x,y);
-title(['La funcion 1 es biyectiva']);
+title(['La funcion 15 es biyectiva']);
 disp("Es subrayectiva por que ningun elemento del codominio sobra respecto al dominio");
 disp("Es inyectiva ya que al trazar una rectar hacia el eje x, no corta mas de un punto la recta");
 disp("Por lo tanto es biyectiva ya que cumple con las anteriores clasificaciones");
