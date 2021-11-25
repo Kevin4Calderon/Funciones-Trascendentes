d787577f995c2692afa6f1da82283ead55632774
Kevin4Calderon <91550461+Kevin4Calderon@users.noreply.github.com>
2021-11-24 20:37:01 -0600

    Create Funcion4.m

---
+++ Funcion4.m
@@ -0,0 +1,19 @@
+% Script de Octave
+% Titulo      :Funciones trascendentes
+% Author      :Kevin Yoan Calderón García
+% Description :Script para graficar una funciones trascendentes
+% Date        :20211123
+% Version     :1
+% Usage       :octave> cd /path/
+% Notes       :Requiere aplicacion octave
+
+clear
+pkg load symbolic
+syms x
+x = 5:1:10
+fx = log(x)/log(8);
+plot(x, fx);
+title(['La funcion 4 es biyectiva']);
+disp("Es subrayectiva por que ningun elemento del codominio sobra respecto al dominio");
+disp("Es inyectiva ya que al trazar una rectar hacia el eje x, no corta mas de un punto la recta");
+disp("Por lo tanto es biyectiva ya que cumple con las anteriores clasificaciones");
